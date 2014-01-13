e28b87a617fbafc531c57388d92eb757b33bdbc7
Angvish Kumar <angvishvish@gmail.com>
2014-01-13 19:49:54 +0530

    Commiting changes for the first time

---
+++ js/rtp-custom-scripts.js
@@ -0,0 +1,11 @@
+/**
+ * Custom Child Scripts
+ */
+
+jQuery( document ).ready( function() {
+    /* Dropdown support for ie7 browsers (li:hover doesn't work in ie7 out of box) */
+    jQuery('.ie7 #rtp-nav-menu li').hover(
+        function() { jQuery(this).children('ul').css('display', 'block'); },
+        function() { jQuery(this).children('ul').css('display', 'none'); }
+    );
+} );
