e89eb50da24fea2cc56aec76ed28ad80b39fb1a9
Angvish Shaw <angvishvish@gmail.com>
2015-03-04 14:01:56 +0530

    updated the contact page

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,5 +1,11 @@
-// "use strict";
+"use strict";
 
-var selected_project = document.getElementById('selected-dropdown');
+function myFunction() {
+  var selected_proj = document.getElementById("selected-dropdown");
+  selected_proj= selected_proj.options[selected_proj.selectedIndex].value;
 
-console.log(selected_project);
+  var iframe_value = document.getElementsByTagName('iframe');
+  for(var frame in iframe_value) {
+    $('iframe').attr('src', selected_proj);
+  }
+}
