d0f6d03879b767be0225d201c5a5371542656c09
angvishvish <angvish@promobitech.com>
2015-03-20 11:33:30 +0530

    adding chomata for image

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,5 +1,24 @@
 "use strict";
 
+var imageElement = document.querySelector('.logo');
+// default options
+var defaults = {
+  colorMode: 'color',
+  compositeOperation: 'lighten',
+  iterationLimit: 0,
+  key: 'high',
+  lineWidth: 0.001,
+  lineMode: 'point',
+  origin: ['right', 'top'],
+  outputSize: 'original',
+  pathFinderCount: 500,
+  speed: 1,
+  turningAngle: Math.PI
+};
+
+var chromata = new Chromata(imageElement, defaults);
+chromata.start();
+
 function myFunction() {
   var selected_proj = document.getElementById("selected-dropdown");
   selected_proj= selected_proj.options[selected_proj.selectedIndex].value;
